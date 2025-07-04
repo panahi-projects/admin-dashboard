1cadc5c46f5ae106302b0a6bcf855e94a3a37b7c
SAEED.PANAHI <panahi.projects@gmail.com>
2025-07-04 15:19:00 +0330

    test(e-commerce/service/product): add test for combined multiple search [GREEN TEST]

--- features/ecommerce/__tests__/services/products.test.ts
+++ features/ecommerce/__tests__/services/products.test.ts
@@ -122,6 +122,14 @@ describe("ProductService finds functionality", () => {
         stock: 10,
         categories: ["electronics", "mobile"],
       },
+      {
+        name: "Wireless Earbuds",
+        price: 29.99,
+        sku: "AUDIO-004",
+        stock: 6,
+        description: "Wireless earbuds",
+        categories: ["electronics", "gadget", "mobile-accessories", "audio"],
+      },
     ]);
   });
 
@@ -131,9 +139,9 @@ describe("ProductService finds functionality", () => {
     const result = await productService.getProducts({}, { page: 1, limit: 2 });
 
     expect(result.data.length).toBe(2);
-    expect(result.total).toBe(4);
+    expect(result.total).toBe(5);
     expect(result.page).toBe(1);
-    expect(result.totalPages).toBe(2);
+    expect(result.totalPages).toBe(3);
     expect(result.hasNextPage).toBeTruthy();
     expect(result.hasPrevPage).toBeFalsy();
   });
@@ -141,14 +149,14 @@ describe("ProductService finds functionality", () => {
   //#2
   it("should filter by category", async () => {
     const result = await productService.getProducts({ category: "audio" });
-    expect(result.data.length).toBe(3);
+    expect(result.data.length).toBe(4);
     expect(result.data.every((p) => p.categories.includes("audio"))).toBe(true);
   });
 
   //#3
   it("should search by text", async () => {
     const result = await productService.getProducts({ search: "wireless" });
-    expect(result.data.length).toBe(1);
+    expect(result.data.length).toBe(2);
     expect(result.data[0].name).toContain("Wireless");
   });
 
@@ -165,7 +173,7 @@ describe("ProductService finds functionality", () => {
   //#5
   it("should filter by stock availability", async () => {
     const inStockResult = await productService.getProducts({ inStock: true });
-    expect(inStockResult.data.length).toBe(3);
+    expect(inStockResult.data.length).toBe(4);
 
     const outOfStockResult = await productService.getProducts({
       inStock: false,
@@ -197,8 +205,26 @@ describe("ProductService finds functionality", () => {
     expect(result.data[0].price).toBe(99.99);
   });
 
-  //FindOne:
   //#7
+  it("should combine multiple query to find products", async () => {
+    const result = await productService.getProducts(
+      {
+        search: "wire",
+        minPrice: 20,
+        inStock: true,
+      },
+      { page: 1, limit: 10 }
+    );
+
+    expect(result).toBeDefined();
+    expect(result.data.length).toBe(2);
+    expect(result.total).toBe(2);
+    expect(result.hasPrevPage).toBeFalsy();
+    expect(result.hasNextPage).toBeFalsy();
+  });
+
+  //FindOne:
+  //#8
   it("should find a product by exact SKU", async () => {
     const product = await productService.getProductByQuery({
       sku: "PHONE-001",
@@ -209,7 +235,7 @@ describe("ProductService finds functionality", () => {
     expect(product?.price).toBe(799.99);
   });
 
-  //#8
+  //#9
   it("should find a product by name regex", async () => {
     const product = await productService.getProductByQuery({
       name: "smartphone",
@@ -219,7 +245,7 @@ describe("ProductService finds functionality", () => {
     expect(product?.sku).toBe("PHONE-001");
   });
 
-  //#9
+  //#10
   it("should return null when no product matches", async () => {
     const product = await productService.getProductByQuery({
       sku: "NON-EXISTENT",
@@ -227,7 +253,7 @@ describe("ProductService finds functionality", () => {
     expect(product).toBeNull();
   });
 
-  //#10
+  //#11
   it("should find a product by price range", async () => {
     const product = await productService.getProductByQuery({
       minPrice: 50,
@@ -236,7 +262,7 @@ describe("ProductService finds functionality", () => {
     expect(product?.sku).toBe("AUDIO-003");
   });
 
-  //#11
+  //#12
   it("should find a product by in-stock", async () => {
     const productInStock = await productService.getProductByQuery({
       inStock: true,
@@ -249,11 +275,11 @@ describe("ProductService finds functionality", () => {
     expect(productNotInStock?.stock).toEqual(0);
   });
 
-  //#12
+  //#13
   it("should combine multiple query criteria", async () => {
     const product = await productService.getProductByQuery({
       category: "audio",
-      minPrice: 20,
+      minPrice: 200,
       inStock: true,
     });
     expect(product?.sku).toBe("AUDIO-001"); // Only the headphones match all criteria
